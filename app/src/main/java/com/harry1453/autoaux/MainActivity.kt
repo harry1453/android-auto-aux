01e657e84ed3166c4745c5f9dc44c766c5cd1b0e
Harry Phillips <harryjamesphillips@gmail.com>
2020-01-08 18:12:33 +0000

    It Works!

--- app/src/main/java/com/harry1453/autoaux/MainActivity.kt
+++ app/src/main/java/com/harry1453/autoaux/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.harry1453.autoaux
 
+import android.media.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.Completable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        feedbackAudio()
+                .subscribeOn(Schedulers.io())
+                .subscribe({ Log.e("AAA", "Complete") }, { Log.e("AAA", "Error", it) })
     }
+
+    private fun feedbackAudio(): Completable {
+        return Completable.create { emitter ->
+            Log.e("AAA", "Starting")
+            val buffer = ByteArray(bufferSizeIn)
+            val audioRecord = AudioRecord(audioSource, sampleRate, channelInConfig, encoding, bufferSizeIn)
+            try {
+                val audioTrack = AudioTrack(audioAttributes, audioFormat, bufferSizeOut, AudioTrack.MODE_STREAM, AudioManager.AUDIO_SESSION_ID_GENERATE)
+                try {
+                    audioRecord.startRecording()
+                    audioTrack.play()
+
+                    Log.e("AAA", "Begin loop")
+                    while (!emitter.isDisposed) {
+                        val readSamples = audioRecord.read(buffer, 0, buffer.size, readMode)
+                        if (readSamples > 0) {
+                            val writeSamples = audioTrack.write(buffer, 0, readSamples, writeMode)
+                            if (writeSamples <= 0) {
+                                Log.e("AAA", "writeSamples was $writeSamples")
+                            }
+                        } else {
+                            Log.e("AAA", "readSamples was $readSamples")
+                        }
+                    }
+                } finally {
+                    audioTrack.release()
+                }
+            } finally {
+                audioRecord.release()
+                Log.e("AAA", "Done")
+            }
+        }
+    }
+
+    private val sampleRate = 48000
+
+    private val readMode = AudioRecord.READ_BLOCKING
+    private val writeMode = AudioTrack.WRITE_BLOCKING
+    private val audioSource = MediaRecorder.AudioSource.MIC
+    private val channelInConfig = AudioFormat.CHANNEL_IN_STEREO
+    private val channelOutConfig = AudioFormat.CHANNEL_OUT_STEREO
+    private val encoding = AudioFormat.ENCODING_PCM_16BIT
+
+    private val bufferSizeIn = AudioRecord.getMinBufferSize(sampleRate, channelInConfig, encoding)
+            .apply {
+                if (this <= 0) error("Error getting min buffer size: $this")
+            }
+
+    private val bufferSizeOut = AudioRecord.getMinBufferSize(sampleRate, channelOutConfig, encoding)
+            .apply {
+                if (this <= 0) error("Error getting min buffer size: $this")
+            }
+
+    private val audioAttributes = AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .build()
+
+    private val audioFormat = AudioFormat.Builder()
+            .setEncoding(encoding)
+            .setSampleRate(sampleRate)
+            .build()
 }
